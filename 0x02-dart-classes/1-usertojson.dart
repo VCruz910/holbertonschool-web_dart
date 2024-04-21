95de53ef9e3d04979c327c942cd6dad6ab16bd9b
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2024-04-21 00:20:25 -0400

    Add files via upload

--- 0x02-dart-classes/1-usertojson.dart
+++ 0x02-dart-classes/1-usertojson.dart
@@ -1,15 +1,13 @@
 class User {
-  String? name;
-  int? age;
-  double? height;
-
-  User({name: String, age: int, height: double}) {
-    this.name = name;
-    this.age = age;
-    this.height = height;
+  User({required this.name, required this.age, required this.height});
+  String name;
+  int age;
+  double height;
+  Map toJson() {
+    return {"name": name, "age": age, "height": height};
   }
 
-  Map toJson() {
-    return {'name': this.name, 'age': this.age, 'height': this.height};
+  String showName() {
+    return "Hello $name";
   }
 }
