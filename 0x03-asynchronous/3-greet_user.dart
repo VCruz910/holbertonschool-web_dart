7276cb57a4b6fe95ee664933eccf20bcc9f0cb14
Victor Samuel Cruz Marrero <vscruz910@gmail.com>
2024-05-01 01:31:12 -0400

    Add files via upload

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,31 +1,27 @@
 import 'dart:convert';
-import 'dart:async';
+import '3-util.dart';
 
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<bool> checkCredentials() =>
-    Future.delayed(const Duration(seconds: 2), () => true);
-    
-greetUser() async {
+Future<String> greetUser() async {
   try {
-  return 'hello ${json.decode(await fetchUserData())['username']}';
-  } catch (err) {
-    print('error caught: $err');
+    String userData = await fetchUserData();
+    Map<String, dynamic> userDataMap = json.decode(userData);
+    return "Hello ${userDataMap['username']}";
+  } catch (error) {
+    return "error caught: $error";
   }
 }
 
-loginUser() async {
- try {
-  if (await checkCredentials()) {
-    return await greetUser();
-  } else {
-    return 'Wrong credentials';
-  }
-} catch (err) {
-    print('error caught: $err');
+Future<String> loginUser() async {
+  try {
+    bool hasUser = await checkCredentials();
+    if (hasUser) {
+      print("There is a user: true");
+      return await greetUser();
+    } else {
+      print("There is a user: false");
+      return "Wrong credentials";
+    }
+  } catch (error) {
+    return "error caught: $error";
   }
 }
